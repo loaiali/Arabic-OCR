b8684ef440b864338989b86d670a6f5dce366c13
Shehab <Shehabalaa97@gmail.com>
2019-12-22 16:49:57 +0200

    Reorganize main and add ability to disable serach

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import sys
+import numpy as np
+import argparse
+import os
 from sklearn.externals import joblib
 from config import modelToPredict
 from segmentation2 import segmentationFromPath, showScaled
 from predict import predictFromImage, activationFunction
-import sys
-import numpy as np
-# sys.path.append("postprocessing/Decoding")
 from postprocessing.Decoding.fst import FST
 from postprocessing.Decoding.beam_search import BeamSearch
 from config import englishName, arabicNames
+from time import time
+# sys.path.append("postprocessing/Decoding")
 '''
 search graph params interface
 [
@@ -18,12 +21,18 @@ search graph params interface
 
 
 class OCR:
-    def __init__(self, decodingGraphPath, inputLabelsPath, lmWeight, beamWidth):
-        self.fst = FST(decodingGraphPath, inputLabelsPath)
-        self.lmWeight = lmWeight
-        self.beamWidth = beamWidth
+    def __init__(self, decodingGraphPath, inputLabelsPath, beamWidth,lmWeight,withSearch):
+        self.withSeach = withSearch
+        if(withSearch):
+            self.lmWeight = lmWeight
+            self.beamWidth = beamWidth
+            self.fst = FST(decodingGraphPath, inputLabelsPath)
+        self.indexToLetters = None
+        with open(inputLabelsPath,"r", encoding="utf8") as f:
+            indexToLetters = {i: englishName[letter] for (i,letter) in enumerate(f.read().split())}
+                
         
-    def getWordsFromImage(self, imagePath):
+    def getTextFromImage(self, imagePath):
         '''
             returns list of:
             [
@@ -42,9 +51,11 @@ class OCR:
         # joblib.dump(img, "img.test")
         # wordsSegmented, img = joblib.load("wordsSegmented.test"), joblib.load("img.test")
 
-        words15 = []
-        wordsCount = 0
         # [      {'rows': (), 'columns': (), 'srl': [{mid: int}]}  ,       {}, {}]
+        wordsCount = 0
+        letters = []
+        maxWordsCount = 1
+
         for wordDictionary in wordsSegmented:
             x1, x2, y1, y2 = *wordDictionary['rows'], *wordDictionary['columns']
             currentWordImage = img[x1:x2, y1:y2]
@@ -60,46 +71,69 @@ class OCR:
                 # showScaled(currentCharImage, "currentCharImage", 100)
                 lettersToScores = predictFromImage(currentCharImage, True)
 
-                # letterclassifer = arabicNames[sorted(lettersToScores.items(), key=lambda x: x[1])[-1][0]]
-                # allImageWords.append(letterclassifer) # TODO : remove this shit
-                # print(letterclassifer)
+                if(self.withSeach):
+                    scoresSorted = [lettersToScores[indexToLetters[i]] for i in range(len(indexToLetters.keys()))]
+                    scoresSorted = activationFunction(scoresSorted)
+                    letters.append(np.array(scoresSorted))
+                else:
+                    predLetter = arabicNames[sorted(lettersToScores.items(), key=lambda x: x[1])[-1][0]]
+                    letters.append(predLetter) 
+
+            wordsCount+=1
+            if (self.withSeach):
+                if(wordsCount >= maxWordsCount):
+                    predictedWords = self._search(np.array(letters))
+                    allImageWords.append(''.join(predictedWords))
+                    letters.clear()
+            else:
+                allImageWords.append(''.join(letters))
+                letters.clear()
+            
                 
-                indexToLetters = None
-                with open("input_labels.txt","r", encoding="utf8") as f:
-                    indexToLetters = {i: englishName[letter] for (i,letter) in enumerate(f.read().split())}
-                scoresSorted = [lettersToScores[indexToLetters[i]] for i in range(len(indexToLetters.keys()))]
-                # print(scoresSorted)
-                scoresSorted = activationFunction(scoresSorted)
-                # print(scoresSorted)
-                words15.append(np.array(scoresSorted))
-            wordsCount += 1
-            if (wordsCount >= 1):
-                # TODO : uncomment this shit
-                predictedWords = self._search(np.array(words15))
-                # print(predictedWords)
-                # yield predictedWords
-                allImageWords.append(predictedWords)
-                wordsCount = 0
-                words15.clear()
-            # showScaled(currentCharImage, 'currentChar', 150)
-        txt = ' '.join([''.join(w) for w in allImageWords])
-        with open("hypo_error.txt", 'w', encoding='utf-8') as f:
-            f.write(txt)
         return ' '.join(allImageWords)
 
 
     def _search(self, predMatrix):
-        # print("searching:")
         words = self.fst.decode(BeamSearch(self.beamWidth), predMatrix, self.lmWeight)
         return words
 
 
 def main():
-    prog = OCR("H1.txt", "input_labels.txt", 10, 250)
-    imagePath = "scanned/capr1.png"
-    words = prog.getWordsFromImage(imagePath)
-    print(words)
+    parser = argparse.ArgumentParser(
+        description="OCR parameters ")
+    parser.add_argument('-search', '--search',
+                        help='Enable serach or not', required=True, type=bool, default=True)
+    parser.add_argument('-graph', '--graph',
+                        help="Text-format openfst decoding graph", required=False, default='LG.txt')
+    parser.add_argument('-lmweight', '--lmweight', help='Relative weight of LM score',
+                        required=False, type=float, default=1)
+    parser.add_argument('-beam_width', '--beam_width',
+                        help='Maximum token count per frame', required=False, type=int, default=250)
+    parser.add_argument('-ilabels', '--ilabels',
+                        help="Text files containing input labels",type=str, required=True, default="input_labels.txt")
+    parser.add_argument('-refPath', '--refPath',
+                        help="Folder continaing refernces text files which are also image files names to run OCR on it",
+                        type=str, required=True, default=None)
+    parser.add_argument(
+        '-predPath', '--predPath', help='path to write output hypotheses', type=str, required=True, default=None)
+    parser.add_argument(
+        '-imgsPath', '--imgsPath', help='Path where scanned images live',type=str, required=False, default='./scanned/')
 
+    args = parser.parse_args()
 
+    prog = OCR(args.graph, args.ilabels,lmWeight=1, beamWidth= 500,withSearch=True)
+
+    for fileName in os.listdir(args.refPath):
+        startTime = time()
+        print("Start image "+ fileName)
+
+        predictedText = prog.getTextFromImage(os.path.join(args.imgsPath, fileName))
+
+        print(f'Image {fileName} took {int(time=startTime)} seconds')
+
+        with open(os.path.join(args.predPath, fileName),encoding="utf-8") as f:
+            f.write(predictedText)
+
+# python main.py -search 0 -graph LG0.txt -lmweight 1 -beam_width 250 -ilabels input_labels.txt -imgsPath ./scanned -refPath ./reference/ -predPath ./predicted
 if __name__ == "__main__":
     main()
