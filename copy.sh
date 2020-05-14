c9e30b0116b667c3bc13f0cbf2ecbeac0e0680c5
Ramy Saied <ramy.m.saied@gmail.com>
2020-05-14 10:12:56 +0200

    add config file

--- copy.sh
+++
@@ -1,5 +0,0 @@
-images=$1
-text=$2
-dest=$3
-
-for file in `ls $images`; do cp $text`echo $file | cut -f1 -d'.'`.txt $dest;done
--- run.bat
+++
@@ -1,9 +0,0 @@
-echo off
-set logFile=runs.log
-set sep=-----------------------------------
-
-for /f "usebackq tokens=1*" %%i in (`echo %*`) DO @ set params=%%j
-set module=%1
-
-echo %DATE% %sep% %module% (%params%): %sep% >> %logFile%
-python %module% %params% >> %logFile%
